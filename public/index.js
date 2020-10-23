daacf3f3ae19bf4c83d9a76d5be63360767e61ab
duong.manh.hoang <duong.manh.hoang@sun-asterisk.com>
2020-10-23 13:21:29 +0700

    fix warning

--- public/index.js
+++ public/index.js
@@ -1,4 +1,6 @@
-const socket = io.connect('https://demo-nodejs-hoang.herokuapp.com');
+// const socket = io.connect('https://demo-nodejs-hoang.herokuapp.com');
+const socket = io.connect('localhost:7000');
+
 let answersFrom = {}, offer;
 
 const peerConnection = window.RTCPeerConnection ||
@@ -24,13 +26,13 @@ navigator.getUserMedia = navigator.getUserMedia ||
 
 const pc = new peerConnection({
     iceServers: [{
-        url: "stun:stun.services.mozilla.com",
+        urls: "stun:stun.services.mozilla.com",
         username: "somename",
         credential: "somecredentials"
     }]
 });
 
-pc.onaddstream = function (obj) {
+pc.ontrack = function (obj) {
     const vid = document.createElement('video');
     vid.setAttribute('class', 'video-small');
     vid.setAttribute('autoplay', 'autoplay');
@@ -72,7 +74,7 @@ socket.on('answer-made', function (data) {
     }, error);
 });
 
-navigator.getUserMedia({ video: true, audio: true }, function (stream) {
+navigator.mediaDevices.getUserMedia({ video: true, audio: true }, function (stream) {
     var video = document.querySelector('video');
     video.srcObject = stream;
     pc.addStream(stream);
